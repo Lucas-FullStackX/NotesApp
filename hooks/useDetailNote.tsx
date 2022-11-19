7acc250430ff4c1e65dfa193cc41da3dc35b2e12
Felipe Morales <carrionfelipe15@gmail.com>
2022-11-19 00:32:15 -0500

    add hook of detail

---
+++ hooks/useDetailNote.tsx
@@ -0,0 +1,46 @@
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import { Database } from '../lib/database.types';
+import { useEffect, useState } from 'react';
+import { PostgrestError } from '@supabase/supabase-js';
+
+type Overwrite<T, U> = Pick<T, Exclude<keyof T, keyof U>> & U;
+export type NoteData = Overwrite<
+  Database['public']['Tables']['notes']['Row'],
+  {
+    patient:
+      | Database['public']['Tables']['patient']['Row'][]
+      | Database['public']['Tables']['patient']['Row'];
+  }
+>;
+type useFetchNotesResponse = {
+  data: Database['public']['Tables']['patient']['Row'];
+  error?: PostgrestError;
+  loading: boolean;
+};
+
+export function useDetailNote({ id }): useFetchNotesResponse {
+  const [error, setError] = useState<useFetchNotesResponse['error']>();
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState<useFetchNotesResponse['data']>();
+  const supabaseClient = useSupabaseClient<Database>();
+
+  useEffect(() => {
+    async function loadData() {
+      setLoading(true);
+      const { error: notesError, data: notesData } = await supabaseClient
+        .from('notes')
+        .select('*')
+        .eq('id', id);
+      if (notesError) {
+        setError(notesError);
+      }
+      if (notesData) {
+        setData(notesData[0]);
+      }
+      setLoading(false);
+    }
+
+    loadData();
+  }, [id]);
+  return { error, loading, data };
+}
--- pages/note/[id].tsx
+++ pages/note/[id].tsx
@@ -1,10 +1,13 @@
 import type { NextPage } from 'next';
 import { Box } from '@mui/system';
 import { useRouter } from 'next/router';
+import { useDetailNote } from '../../hooks/useDetailNote';
 
 const NoteDetail: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
+  const { data } = useDetailNote({ id });
+  console.log(data, 'id');
   return <Box m={3}>{id}</Box>;
 };
 
