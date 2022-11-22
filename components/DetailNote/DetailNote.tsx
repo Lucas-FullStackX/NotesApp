f3c557eb0503385ca9c6dc07abfa4438417b9b07
Felipe Morales <carrionfelipe15@gmail.com>
2022-11-21 22:27:16 -0500

    Skeleton Detail Note

--- components/DetailNote/DetailNote.tsx
+++ components/DetailNote/DetailNote.tsx
@@ -1,6 +1,8 @@
 import { Box, Paper, Stack, Typography, Grid } from '@mui/material';
 import { NoteDetailType } from '../../hooks/useDetailNote';
 import { humanizeDate } from '../../src/utils';
+import Skeleton from '@mui/material/Skeleton';
+
 // import { getImageURL } from '../../src/utils/index';
 
 export default function DetailNote({
@@ -8,207 +10,232 @@ export default function DetailNote({
 }: {
   data: NoteDetailType;
 }): JSX.Element {
-  return (
-    <Box>
-      <Typography
-        variant="h5"
-        component="div"
-        fontWeight="bold"
-        sx={{
-          m: 2
-        }}
-      >
-        {Array.isArray(data?.patient)
-          ? data?.patient[0].name
-          : data?.patient?.name ?? 'Sin Paciente'}
-      </Typography>
-      <Paper
-        sx={{
-          m: 2
-        }}
-        elevation={2}
-      >
-        <Stack spacing={1} p={2}>
-          <Box>
-            <Typography variant="h6" component="div" fontWeight={400}>
-              Estado
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {data?.general_state ?? ''}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="div" fontWeight={400}>
-              Creado
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {humanizeDate(data?.created_at) ?? ''}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="div" fontWeight={400}>
-              Estado anémico
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {data?.anemic_state ?? ''}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="div" fontWeight={400}>
-              Deposición
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {data?.deposition ?? ''}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="div" fontWeight={400}>
-              Diéresis
-            </Typography>
-          </Box>
-          <Typography variant="body1" color="text.secondary">
-            {data?.dieresis ?? ''}
-          </Typography>
-          <Box>
-            <Typography variant="h6" component="div" fontWeight={400}>
-              Emesis
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {data?.emesis ?? ''}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="div" fontWeight={400}>
-              Caidas
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {data?.falls ?? ''}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="div" fontWeight={400}>
-              Alimentacion
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {data?.food ?? ''}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="div" fontWeight={400}>
-              Medicinas
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {data?.medicines ?? ''}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="div" fontWeight={400}>
-              Observaciones
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {data?.news ?? ''}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="div" fontWeight={400}>
-              Protesis
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {data?.prosthesis ?? ''}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="div" fontWeight={400}>
-              Sueño
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {data?.sleep ?? ''}
-            </Typography>
-          </Box>
-          <Box>
-            <Typography variant="h6" component="div" fontWeight={400}>
-              Deambulación
-            </Typography>
-            <Typography variant="body1" color="text.secondary">
-              {data?.wandering ?? ''}
-            </Typography>
-          </Box>
-        </Stack>
-      </Paper>
-      <Paper
-        sx={{
-          m: 2,
-          p: 2
-        }}
-        elevation={2}
-      >
+  console.log(data?.patient, 'a');
+  if (Array.isArray(data?.patient)) {
+    <div>Nashe</div>;
+  } else {
+    if (data?.patient?.name) {
+      return (
         <Box>
-          <Typography variant="h6" component="div" fontWeight={700}>
-            Signos Vitales
-          </Typography>
-          <Grid
-            container
-            rowSpacing={1}
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+          <Typography
+            variant="h5"
+            component="div"
+            fontWeight="bold"
+            sx={{
+              m: 2
+            }}
           >
-            <Grid item xs={6}>
-              Frecuencia cardiaca
+            {data?.patient[0]?.name ?? data?.patient.name}
+          </Typography>
+          <Paper
+            sx={{
+              m: 2
+            }}
+            elevation={2}
+          >
+            <Stack spacing={1} p={2}>
+              <Box>
+                <Typography variant="h6" component="div" fontWeight={400}>
+                  Estado
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  {data?.general_state ?? ''}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" component="div" fontWeight={400}>
+                  Creado
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  {humanizeDate(data?.created_at) ?? ''}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" component="div" fontWeight={400}>
+                  Estado anémico
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  {data?.anemic_state ?? ''}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" component="div" fontWeight={400}>
+                  Deposición
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  {data?.deposition ?? ''}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" component="div" fontWeight={400}>
+                  Diéresis
+                </Typography>
+              </Box>
               <Typography variant="body1" color="text.secondary">
-                {Array.isArray(data?.signs)
-                  ? data?.signs[0].cardiac_frequency
-                  : data?.signs?.cardiac_frequency ?? ''}
+                {data?.dieresis ?? ''}
               </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              Presion sanguinea
-              <Typography variant="body1" color="text.secondary">
-                {Array.isArray(data?.signs)
-                  ? data?.signs[0].sanguine_pressure
-                  : data?.signs?.sanguine_pressure ?? ''}
+              <Box>
+                <Typography variant="h6" component="div" fontWeight={400}>
+                  Emesis
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  {data?.emesis ?? ''}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" component="div" fontWeight={400}>
+                  Caidas
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  {data?.falls ?? ''}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" component="div" fontWeight={400}>
+                  Alimentacion
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  {data?.food ?? ''}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" component="div" fontWeight={400}>
+                  Medicinas
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  {data?.medicines ?? ''}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" component="div" fontWeight={400}>
+                  Observaciones
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  {data?.news ?? ''}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" component="div" fontWeight={400}>
+                  Protesis
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  {data?.prosthesis ?? ''}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" component="div" fontWeight={400}>
+                  Sueño
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  {data?.sleep ?? ''}
+                </Typography>
+              </Box>
+              <Box>
+                <Typography variant="h6" component="div" fontWeight={400}>
+                  Deambulación
+                </Typography>
+                <Typography variant="body1" color="text.secondary">
+                  {data?.wandering ?? ''}
+                </Typography>
+              </Box>
+            </Stack>
+          </Paper>
+          <Paper
+            sx={{
+              m: 2,
+              p: 2
+            }}
+            elevation={2}
+          >
+            <Box>
+              <Typography variant="h6" component="div" fontWeight={700}>
+                Signos Vitales
               </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              Saturacion
-              <Typography variant="body1" color="text.secondary">
-                {Array.isArray(data?.signs)
-                  ? data?.signs[0].saturation
-                  : data?.signs?.saturation ?? ''}
-              </Typography>
-            </Grid>
-            <Grid item xs={6}>
-              Temperatura
-              <Typography variant="body1" color="text.secondary">
-                {Array.isArray(data?.signs)
-                  ? data?.signs[0].temperature + '°'
-                  : data?.signs?.temperature + '°' ?? ''}
-              </Typography>
-            </Grid>
-            <Grid item xs={12}>
-              Observaciones
-              <Typography variant="body1" color="text.secondary">
-                {Array.isArray(data?.signs)
-                  ? data?.signs[0].news
-                  : data?.signs?.news ?? ''}
-              </Typography>
-            </Grid>
-          </Grid>
+              <Grid
+                container
+                rowSpacing={1}
+                columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+              >
+                <Grid item xs={6}>
+                  Frecuencia cardiaca
+                  <Typography variant="body1" color="text.secondary">
+                    {Array.isArray(data?.signs)
+                      ? data?.signs[0].cardiac_frequency
+                      : data?.signs?.cardiac_frequency ?? ''}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  Presion sanguinea
+                  <Typography variant="body1" color="text.secondary">
+                    {Array.isArray(data?.signs)
+                      ? data?.signs[0].sanguine_pressure
+                      : data?.signs?.sanguine_pressure ?? ''}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  Saturacion
+                  <Typography variant="body1" color="text.secondary">
+                    {Array.isArray(data?.signs)
+                      ? data?.signs[0].saturation
+                      : data?.signs?.saturation ?? ''}
+                  </Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  Temperatura
+                  <Typography variant="body1" color="text.secondary">
+                    {Array.isArray(data?.signs)
+                      ? data?.signs[0].temperature + '°'
+                      : data?.signs?.temperature + '°' ?? ''}
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  Observaciones
+                  <Typography variant="body1" color="text.secondary">
+                    {Array.isArray(data?.signs)
+                      ? data?.signs[0].news
+                      : data?.signs?.news ?? ''}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Box>
+          </Paper>
+          <Typography
+            variant="h6"
+            component="div"
+            fontWeight={700}
+            sx={{
+              m: 2
+            }}
+          >
+            Firma del asistente:
+          </Typography>
+          {data?.assistant && (
+            <Box sx={{ paddingLeft: '15px' }}>
+              {data?.assistant ? (
+                <img src={data?.assistant} alt="asistente" />
+              ) : (
+                ''
+              )}
+            </Box>
+          )}
         </Box>
-      </Paper>
-      <Typography
-        variant="h6"
-        component="div"
-        fontWeight={700}
-        sx={{
-          m: 2
-        }}
-      >
-        Firma del asistente:
-      </Typography>
-      {data?.assistant && (
+      );
+    } else {
+      return (
         <Box>
-          {data?.assistant ? <img src={data?.assistant} alt="asistente" /> : ''}
+          <Skeleton
+            width="50px"
+            sx={{ margin: '12px', height: '50px' }}
+          ></Skeleton>
+          <Skeleton
+            variant="rectangular"
+            width="98%"
+            sx={{ marginLeft: '12px', marginBottom: '5px' }}
+          >
+            <div style={{ paddingTop: '57%' }} />
+          </Skeleton>
         </Box>
-      )}
-    </Box>
-  );
+      );
+    }
+  }
 }
