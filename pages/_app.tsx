2db73cf3196738bf01dc673f948081ad5b19d129
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2022-11-24 21:46:40 -0500

    add pwa config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -30,6 +30,30 @@ export default function MyApp(props: MyAppProps) {
     >
       <CacheProvider value={emotionCache}>
         <Head>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="description" content="Description" />
+          <meta name="keywords" content="Keywords" />
+
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="/icons/favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="/icons/favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
+          <meta name="theme-color" content="#009688" />
           <title>Reverde Ser</title>
           <meta name="viewport" content="initial-scale=1, width=device-width" />
         </Head>
